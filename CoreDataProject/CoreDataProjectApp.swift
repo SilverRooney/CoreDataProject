58d71b81c6bfa1d519ad533d9b646b758e94fc1c
SilverRooney <silver.rooney.dev@gmail.com>
2022-07-07 15:44:38 -0700

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Silver on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
